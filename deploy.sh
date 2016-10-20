8d7642389304939ec5c474299e8d3dbfaee881dc
Joe Karasek <joseph.karasek@gmail.com>
2016-10-20 11:57:20 -0700

    play with deploy script and massage slide background postionsing"

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,14 @@ if [[ $1 == "" ]]; then
   exit 1
 fi
 
+if [[ $2 == "" ]]; then
+  echo "Must supply a passphrase for the server"
+  exit 2
+fi
+
 DEPLOY_HOST='danlinn@wf-198-58-114-22.webfaction.com'
 DEPLOY_PATH="/home/danlinn/webapps/$1"
 
-ssh $DEPLOY_HOST "cd $DEPLOY_PATH && git pull"
+#ssh $DEPLOY_HOST "cd $DEPLOY_PATH && git pull && $2"
+echo $DEPLOY_HOST
+echo $DEPLOY_PATH
